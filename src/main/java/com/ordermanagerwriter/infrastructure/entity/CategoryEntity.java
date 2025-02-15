6bf794569e7718728c0dff965b0689a31d117293
Arthur Lopes <arthurlopr12@gmail.com>
2025-02-15 18:31:34 -0300

    feat: add category model and entity

---
+++ src/main/java/com/ordermanagerwriter/infrastructure/entity/CategoryEntity.java
@@ -0,0 +1,27 @@
+package com.ordermanagerwriter.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+public class CategoryEntity {
+
+    @Id
+    @Column(name = "category_id")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "emoji")
+    private String emoji;
+}
